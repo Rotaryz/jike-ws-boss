903fa7c72337dc173469363be75a90d9f2b203e4
Ben <ben@jkweixin.com>
2018-07-20 22:25:48 +0800

    微商雷达发布上线

--- src/common/js/oauth.js
+++ src/common/js/oauth.js
@@ -2,7 +2,7 @@ const env = process.env
 
 export default class Oauth {
   constructor() {
-    const appId = env.NODE_ENV === 'production' ? 'ww65ff1d66710fd8c5' : 'wwc1da73fa86deeb72'
+    const appId = env.NODE_ENV === 'production' ? 'wwa05504832a7488be' : 'ww90eca816eb9b3cf9'
     const uri = location.origin + '/oauth'
     const redirectUri = encodeURIComponent(uri)
     this.oauthUri = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUri}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
