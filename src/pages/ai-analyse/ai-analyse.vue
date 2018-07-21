2cd64a97e2d646afee5e866992ef728a2d971189
wengtianliang <wengtianliang@jkweixin.com>
2018-07-21 18:58:48 +0800

    fix css

--- src/pages/ai-analyse/ai-analyse.vue
+++ src/pages/ai-analyse/ai-analyse.vue
@@ -1,18 +1,16 @@
 <template>
   <article class="ai-analyse">
-    <div class="analyse-box">
-      <scroll ref="scroll"
-              v-if="dataArray.length"
-              bcColor="#fff"
-              :data="dataArray"
-              :pullUpLoad="pullUpLoadObj"
-              @pullingUp="onPullingUp"
-      >
-        <div class="user-card-box" v-for="(item,index) in dataArray" :key="index" @click="toCapacityModel(item)">
-          <user-card :cardInfo="item" :idx="index" useType="ai"></user-card>
-        </div>
-      </scroll>
-    </div>
+    <scroll ref="scroll"
+            v-if="dataArray.length"
+            bcColor="#fff"
+            :data="dataArray"
+            :pullUpLoad="pullUpLoadObj"
+            @pullingUp="onPullingUp"
+    >
+      <div class="user-card-box" v-for="(item,index) in dataArray" :key="index" @click="toCapacityModel(item)">
+        <user-card :cardInfo="item" :idx="index" useType="ai"></user-card>
+      </div>
+    </scroll>
     <section class="exception-box" v-if="isEmpty">
       <exception errType="nodata"></exception>
     </section>
@@ -136,17 +134,13 @@
 
   .exception-box
     padding-top: 137px
+
   .ai-analyse
     fill-box()
     overflow: hidden
-    .ai-analyse
-      position: fixed
-      top: 62px
-      bottom: 50px
-      left: 0
-      right: 0
-      overflow: hidden
-      background-color: $color-white-fff
+    top: 62px
+    bottom: 50px
+    background-color: $color-white-fff
     .user-card-box
       height: 75px
       padding-left: 15px
